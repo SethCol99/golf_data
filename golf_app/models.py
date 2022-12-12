4bed751d51401ecca82b7046685d5ded7dad61e4
SethCol99 <sd17colu@siena.edu>
2022-12-11 23:37:30 -0500

    models mostly implemented, calcultions done, i think, also members for each round modified

--- golf_app/models.py
+++ golf_app/models.py
@@ -4,9 +4,9 @@ import datetime
 
 NINE = 9
 EIGHTEEN = 18
-MINOR = 5.00
-ADULT = 15
-SENIOR = 10
+MINOR = 5
+ADULT = 8
+SENIOR = 7
 CARTP = 15
 GOLF_BALLS = 2
 CLUBS = 7
@@ -21,37 +21,63 @@ class golfer(models.Model):
     def __str__ (self):
         return "( " + self.fname + " " + self.lname + " )" + self.phone + ", " + self.age
 
+class options(models.Model):
+
+    occurance = models.ForeignKey(round, on_delete=models.CASCADE)
+    phone = models.ForeignKey(round, on_delete=models.CASCADE)
+    cart = models.IntegerField(maximum = 2, default = 0)
+    golf_balls = models.IntegerField(default=0)
+    club_rentals = models.IntegerField(default=0)
+    
+
+    
 class round(models.Model):
 
     occurance = models.DateTimeField(default = datetime.datetime.now)
     phone = models.ForeignKey(golfer, on_delete=models.CASCADE)
     holes = models.IntegerField(max_length= 2, 
-    choices=[9, 18], default = 9)
-    mem1 = models.IntegerField(default = None)
-    mem2 = models.IntegerField(default = None)
-    mem3 = models.IntegerField(default = None)
+    choices=[NINE, EIGHTEEN], default = 9)
+    mem = models.IntegerField(default = None)
     price = models.FloatField(default = 0.0)
    
     def cal_price(self):
       curr = 0.0
-    
-      if self.round.holes == 9:
-        curr += 9.0
 
-      elif self.round.holes == 18:
-        
-    
-      if self.round.mem1 < 18:
+      if(self.holes == NINE):
+        if self.golfer.age < 18:
+          curr += 5
+        elif self.golfer.age > 18 & self.golfer.age < 65:
+          curr += 8
+        elif self.golfer.age > 65:
+          curr += 7
 
+        if self.round.mem < 18:
+          curr += 5
+        elif self.round.mem > 18 & self.round.mem < 65:
+          curr += 8
+        elif self.round.mem > 65:
+          curr += 7
+
+        elif self.round.holes == 18:
+          if self.golfer.age < 18:
+           curr += 10
+          elif self.golfer.age > 18 & self.golfer.age < 65:
+           curr += 16
+          elif self.golfer.age > 65:
+           curr += 14
+
+          if self.round.mem < 18:
+            curr += 10
+          elif self.round.mem > 18 & self.round.mem < 65:
+            curr += 16
+          elif self.round.mem > 65:
+            curr += 14
+
+        curr += self.golf_balls * GOLF_BALLS
+        curr += self.cart * CARTP
+        curr += self.club_rentals * CLUBS
 
         price = curr
 
     def __str__ (self):
-        return "( " + self.occurance + " " + self.phone + " )" + self.holes + ", " + 
-
-class options(models.Model):
-    cart = models.IntegerField(maximum = 2, default = 0)
-    golf_balls = models.IntegerField(default=0)
-    club_rentals = models.IntegerField(default=0)
-    
-    
+        return "( " + self.occurance + " " + self.phone + " )" + self.holes + ", " + self.mem1 + ", " + self.mem2 + ", " + self.mem3 + ", "
