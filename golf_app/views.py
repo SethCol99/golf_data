a07d89792799f83308274c3ef6e80c14ca1a8933
SethCol99 <sd17colu@siena.edu>
2022-12-12 10:54:45 -0500

    some path changes made to urls and views

--- golf_app/views.py
+++ golf_app/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
-
+from .models import golfer, options, round
+from django.views.generic import ListView
 # Create your views here.
+def landing(request):
+    return render(request, 'golf_data/landing.html')
+
+class round(ListView):
+    model = round
+    template = 'golf_data/round.html'
