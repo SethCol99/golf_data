7e8db054c74f6cbf2ee77cb021406ae099ac208f
SethCol99 <sd17colu@siena.edu>
2022-12-11 20:08:38 -0500

    models for golf course database started

--- golf_app/models.py
+++ golf_app/models.py
@@ -1,3 +1,13 @@
 from django.db import models
-
+import datetime
 # Create your models here.
+
+NINE = 9
+EIGHTEEN = 18
+
+class golfer(models.Model):
+
+    fname = models.CharField(max_length= 10)
+    lname = models.CharField(max_length= 15)
+    phone = models.CharField(max_length= 13)
+    
